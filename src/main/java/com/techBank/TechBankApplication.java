13e6500c5c075f144a421e5dabeb27bf15a499c6
Nicolas Potier <nicolaspotier97@gmail.com>
2021-05-24 09:54:00 -0500

    first commit

---
+++ src/main/java/com/techBank/TechBankApplication.java
@@ -0,0 +1,13 @@
+package com.techBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techBank/TechBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
